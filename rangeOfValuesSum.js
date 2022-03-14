63b7f591229116661cbe70b2403fbe078e3c5230
andreihaa <82822724+andreihaa@users.noreply.github.com>
2022-03-14 16:52:32 +0200

    Update rangeOfValuesSum.js

--- rangeOfValuesSum.js
+++ rangeOfValuesSum.js
@@ -3,9 +3,33 @@ function calculateSum(arr){
     for(let i=arr[0]; i <= arr[1]; i++){
         sum = sum + i; 
     }
-    console.log(sum); 
 }
 
-calculateSum([1,9])// console.log(sumAll2([1, 4]));
+console.log(calculateSum([1,9]))
+
+
+function sumAll(arr) {
+    if(arr[0] < arr[1]){
+        for(let i=arr[0]; i<=arr[1]; i++){
+        sum = sum + i; 
+        }
+    }
+    else{
+        for(let i=arr[1]; i<=arr[0]; i++){
+        sum = sum + i; 
+        }
+    }
+}
+
+function sumAll2(arr) {
+    let sum = 0; 
+    const sorted = arr.sort();
+    for(let i=sorted[0]; i<=sorted[1]; i++){
+      sum = sum + i; 
+    }
+    return sum; 
+  }
+  
+// console.log(sumAll2([1, 4]));
 console.log(sumAll2([5, 10]));
 console.log(sumAll2([10, 5]));
